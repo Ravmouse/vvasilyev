8c335f40ea5670d7363eabc3b37b86ab613d238e
Vitaly Vasilyev <rav.energ@rambler.ru>
2017-08-03 15:56:58 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+* Класс Calculate.
+*/
 public class Calculate {
+	
+	/**
+	* Точка входа в программу. 
+	* Метод println() выводит строку "Hello, World!" в консоль. 
+	*/
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculate task.
+*
+* @author Vitaly Vasilyev (mailto:rav.energ@rambler.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
