ddbf5c7e0937d6a6723ba0ac2053ca475afb81c3
Vitaly Vasilyev <rav.energ@rambler.ru>
2017-08-07 23:38:16 +0300

    Unknown symbol

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,57 +1,56 @@
 package ru.job4j.calculator;
 /**
-* ʊ���� Calculator.
-*
-* @author Vitaly Vasilyev (rav.energ@rambler.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Класс Calculator.
+ * @author Vitaly Vasilyev (rav.energ@rambler.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculator {
     /**
-    * ����� �������� ��������� ����������.
-    */
+     * Переменная для хранения результата вычисления.
+     */
     private double result;
 
     /**
-    * �������� ���� �����.
-    * @param first - first
-    * @param second - second
-    */
+     * Метод сложения.
+     * @param first - first
+     * @param second - second
+     */
     void add(double first, double second) {
         this.result = first + second;
     }
 
     /**
-    * ��������� ���� �����.
-    * @param first - first
-    * @param second - second
-    */
+     * Метод вычитания.
+     * @param first - first
+     * @param second - second
+     */
     void sub(double first, double second) {
         this.result = first - second;
     }
 
     /**
-    * ������� ������� ����� �� ������ �����.
-    * @param first - first
-    * @param second - second
-    */
+     * Метод деления.
+     * @param first - first
+     * @param second - second
+     */
     void div(double first, double second) {
         this.result = first / second;
     }
 
     /**
-    * ��������� ������� ����� �� ������ �����.
-    * @param first - first
-    * @param second - second
-    */
+     * Метод умножения.
+     * @param first - first
+     * @param second - second
+     */
     void mult(double first, double second) {
         this.result = first * second;
     }
 
     /**
-    * ���������� ��������� �������������� ��������.
-    * @return double
-    */
+     * Метод получения результата вычисления.
+     * @return double
+     */
     public double getResult() {
         return this.result;
     }
