c36a69f3a5d12eec8ae93cf01414ac5cb35abaa3
Vitaly Vasilyev <rav.energ@rambler.ru>
2017-10-22 14:11:57 +0300

    5.3.4. Задан связанный список. Определить цикличность (исправления - 2)

--- chapter_005/src/main/java/ru/job4j/h3list/cycle/ListWithCycle.java
+++ chapter_005/src/main/java/ru/job4j/h3list/cycle/ListWithCycle.java
@@ -7,22 +7,27 @@ public class ListWithCycle<T> {
      * The first element in the list.
      */
     Node<T> first;
+
     /**
-     * @return true or false.
+     * Проверяет наличие зацикленности в контейнере, реализующем связанный список.
+     * @return true, если узлы зациклены в середине или на начало списка, и false, если список не замкнут.
      */
     public boolean hasCycle() {
         boolean result = false;
         Node<T> current = first.next;
-        Node<T> beforeCurrent = first;
-        while ((current != first) && (current != null)) {
+        Node<T> before = first;
+        while ((current != before) && (current != first) && (current.next != null)) {
             current = current.next;
-            if (beforeCurrent != current) {
-                beforeCurrent = beforeCurrent.next;
+            if (current != before) {
+                before = before.next;
             } else {
                 break;
             }
+            if ((current != first) && (current.next != null)) {
+                current = current.next;
+            }
         }
-        if ((current == first) || (current == beforeCurrent)) {
+        if ((current == first) || (current == before)) {
             result = true;
         }
         return result;
--- chapter_005/src/test/java/ru/job4j/h3list/cycle/ListWithCycleTest.java
+++ chapter_005/src/test/java/ru/job4j/h3list/cycle/ListWithCycleTest.java
@@ -26,21 +26,29 @@ public class ListWithCycleTest {
     }
 
     /**
-     * Checks if the node of the list is referenced to the previous element.
+     * Checks if the node of the list is referenced to the first element or the element that is located
+     * in the middle of the list.
      */
     @Test
     public void cyclingOnTheMiddleElementTest() {
-        Node<Integer> firstNode = new Node<>(10);
-        Node<Integer> secondNode = new Node<>(20);
-        Node<Integer> thirdNode = new Node<>(30);
-        Node<Integer> fourthNode = new Node<>(40);
+        Node<Integer> firstN = new Node<>(10);
+        Node<Integer> secondN = new Node<>(20);
+        Node<Integer> thirdN = new Node<>(30);
+        Node<Integer> fourthN = new Node<>(40);
+        Node<Integer> fifthN = new Node<>(50);
+        Node<Integer> sixthN = new Node<>(60);
+        Node<Integer> seventhN = new Node<>(70);
 
         ListWithCycle<Integer> list = new ListWithCycle<>();
-        list.first = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        thirdNode.next = secondNode;
-        fourthNode.next = null;
+        list.first = firstN;
+        firstN.next = secondN;
+        secondN.next = thirdN;
+        thirdN.next = fourthN;
+        fourthN.next = fifthN;
+        fifthN.next = sixthN;
+        sixthN.next = secondN;
+        seventhN.next = null;
+        System.out.println(list.hasCycle());
         assertThat(list.hasCycle(), is(true));
     }
 }
