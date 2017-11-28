47413f533b9e214f5061de4712708fedca20bdd1
Ravmouse <rav.energ@rambler.ru>
2017-11-28 09:05:49 +0300

    2. Не перекрывать equals и hashCode [#1005].

---
+++ chapter_005/src/main/java/ru/job4j/h5map/t2noequalsandhashcode/package-info.java
@@ -0,0 +1,6 @@
+/**
+* В этом пакете создается класс MapTest с двумя User'ами с одинаковыми полями.
+* @author Vitaly Vasilyev (rav.energ@rambler.ru).
+* @version 1.0
+*/
+package ru.job4j.h5map.t2noequalsandhashcode;
