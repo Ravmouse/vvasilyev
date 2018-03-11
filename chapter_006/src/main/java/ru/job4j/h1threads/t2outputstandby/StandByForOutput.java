193aaf878a8538ccad0dd7e97a7ce3408e30ef91
Vitaly Vasilyev <rav.energ@rambler.ru>
2018-03-12 00:04:54 +0300

    2. Сделать ожидание вывода [#1017].

---
+++ chapter_006/src/main/java/ru/job4j/h1threads/t2outputstandby/StandByForOutput.java
@@ -0,0 +1,42 @@
+package ru.job4j.h1threads.t2outputstandby;
+import ru.job4j.h1threads.t1wordsandspaces.CountWordsAndSpaces;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * A class where two additional threads are created. After that they join to the main thread.
+ */
+public class StandByForOutput extends CountWordsAndSpaces {
+
+    /**
+     * @param text is a parameter to be assigned.
+     */
+    public StandByForOutput(String text) {
+        super(text);
+    }
+
+    /**
+     * @param args args.
+     */
+    public static void main(String[] args) {
+        System.out.println("*** Greetings, User! ***");
+        System.out.println("This is a program that counts a number of spaces and words in a text.");
+        String str;
+        Thread spaceThread;
+        Thread wordThread;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Please, enter a text: ");
+            str = br.readLine();
+            spaceThread = new Thread(new StandByForOutput(str).new CountSpaces());
+            wordThread = new Thread(new StandByForOutput(str).new CountWords());
+            spaceThread.start();
+            wordThread.start();
+
+            spaceThread.join();
+            wordThread.join();
+            System.out.println("Exiting program.\n*** Good Buy, User! ***");
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
---
+++ chapter_006/src/main/java/ru/job4j/h1threads/t2outputstandby/package-info.java
@@ -0,0 +1,9 @@
+/**
+* В этом пакете - класс, расширяющий класс из пред.пакета.
+* Здесь два доп.потока присоединяются к ожидающему их основному потоку.
+* There's the class that extends the class from the previous package.
+* Here the main thread waits for two additional threads to be joined.
+* @author Vitaly Vasilyev (rav.energ@rambler.ru).
+* @version 1.0
+*/
+package ru.job4j.h1threads.t2outputstandby;
