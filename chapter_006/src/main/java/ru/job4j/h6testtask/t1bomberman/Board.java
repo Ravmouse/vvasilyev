bef731bbf329452f6d42d1b71b003da7fb7374d9
Vitaly Vasilyev <rav.energ@rambler.ru>
2018-06-09 22:55:15 +0300

    Игра бомбермен [#1108] (исправления).

--- chapter_006/src/main/java/ru/job4j/h6testtask/t1bomberman/Board.java
+++ chapter_006/src/main/java/ru/job4j/h6testtask/t1bomberman/Board.java
@@ -1,5 +1,4 @@
 package ru.job4j.h6testtask.t1bomberman;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,8 +10,13 @@ public class Board {
      * Массив локов.
      */
     private final ReentrantLock[][] board;
+    /**
+     * Массив ячеек.
+     */
+    private volatile Cell[][] cells;
 
     /**
+     * В конструкторе происходит создание двух массивов: массива ReentrantLock и массива Cell.
      * @param size размер поля.
      */
     public Board(int size) {
@@ -22,66 +26,139 @@ public class Board {
                 board[i][j] = new ReentrantLock();
             }
         }
-    }
-
-    public boolean move(Cell source, Cell dest) throws InterruptedException {
-        boolean rsl = false;
-            if (board[dest.x][dest.y].tryLock(500, TimeUnit.MILLISECONDS)) {
-                try {
-                    rsl = true;
-                } finally {
-                    board[source.x][source.y].unlock();
-                }
-                Thread.sleep(1000);
-            }
-        return rsl;
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        int size = 100;
-        Board b = new Board(size);
-        Cell[][] cells = new Cell[size][size];
-        for (int i = 0; i < cells.length; i++) {
-            for (int j = 0; j < cells[i].length; j++) {
+        this.cells = new Cell[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 cells[i][j] = new Cell(i, j);
             }
         }
-        Thread t1 = new Thread(() -> {
-            int rand;
-            int cX = 40;
-            int cY = 40;
-            Cell source = cells[cX][cY];
-            Cell dest;
-            b.board[source.x][source.y].lock();
-            while (true) {
-                rand = ThreadLocalRandom.current().nextInt(2);
-                if (System.currentTimeMillis() % 2 == 0) {
-                    cX += rand;
-                    dest = cells[cX][cY];
-                } else {
-                    cY += rand;
-                    dest = cells[cX][cY];
-                }
+    }
+
+    /**
+     * @return ссылку на массив ячеек. Нужен для класса Hero.
+     */
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    /**
+     * @return ссылку на массив локов. Нужен для класса Hero.
+     */
+    public ReentrantLock[][] getBoard() {
+        return board;
+    }
+
+    /**
+     * @param source ячейка, откуда нужно "ходить".
+     * @param dest ячейка, куда нужно "пойти".
+     * @return true, если на ячейку dest можно "пойти", и false - если нет.
+     * @throws InterruptedException в случае возникновения исключения.
+     */
+    public boolean move(Cell source, Cell dest) throws InterruptedException {
+        boolean result = false;
+            if (board[dest.getX()][dest.getY()].tryLock(500, TimeUnit.MILLISECONDS)) {
                 try {
-                    if (b.move(source, dest)) {
-                        source = dest;
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    result = true;
+                } finally {
+                    board[source.getX()][source.getY()].unlock();
                 }
+                Thread.sleep(3100);
             }
-        });
-        t1.start();
-        t1.join();
+        return result;
+    }
+
+    /**
+     * @param size размер поля.
+     * @param heroes ссылка на переменное число экземпляров класса Hero.
+     */
+    private void drawTheBoard(int size, Hero... heroes) {
+        int i, j, k, empty;
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                empty = 0;
+                for (k = 0; k < heroes.length; k++) {
+                    if ((heroes[k].getSource().getX() == i) && (heroes[k].getSource().getY() == j)) {
+                        System.out.print(heroes[k].getName());
+                        break;
+                    } else {
+                        empty++; //Если на клетке [i, j] нет ни одного объекта Hero, то увеличить переменную.
+                    }
+                }
+                if (empty == heroes.length) { //Если переменная равна числу аргументов heroes, то нарисовать ОДИН раз, а
+                    System.out.print(".");  //не столько, сколько аргументов в heroes. Чтобы размер поля не увеличивался
+                }                           // в 2-3 раза.
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /**
+     * @param x значение для проверки.
+     * @param y значение для проверки.
+     * @return true, если x или y не выходят за пределы поля и false, если - выходят.
+     */
+    public boolean isItSafe(int x, int y) {
+        boolean result = true;
+        if ((x < 0) || (x >= board.length) || (y < 0) || (y >= board.length)) {
+            result = false;
+        }
+        return result;
+    }
+
+    /**
+     * @param args .
+     * @throws InterruptedException .
+     */
+    public static void main(String[] args) throws InterruptedException {
+        int size = 5;
+        Board b = new Board(size);
+        Hero h1 = new Hero("B", 0, 0, b);
+        h1.start();
+        Hero h2 = new Hero("M", 4, 4, b);
+        h2.start();
+        Hero h3 = new Hero("M", 4, 0, b);
+        h3.start();
+        while (true) {
+            b.drawTheBoard(size, h1, h2, h3);
+            Thread.sleep(3000);
+        }
     }
 }
 
+/**
+ * Класс Cell.
+ */
 class Cell {
-    public final int x;
-    public final int y;
+    /**
+     * Положение по оси x.
+     */
+    private final int x;
+    /**
+     * Положение по оси y.
+     */
+    private final int y;
 
-    public Cell(int x, int y) {
+    /**
+     * @param x положение по оси x при создании объекта.
+     * @param y положение по оси y при создании объекта.
+     */
+    Cell(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * @return положение по оси x.
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return положение по оси y.
+     */
+    public int getY() {
+        return y;
     }
 }
--- chapter_006/src/main/java/ru/job4j/h6testtask/t1bomberman/Hero.java
+++ chapter_006/src/main/java/ru/job4j/h6testtask/t1bomberman/Hero.java
@@ -1,73 +1,132 @@
 package ru.job4j.h6testtask.t1bomberman;
+import java.util.Scanner;
 
 /**
  * Класс героя.
  */
-public class Hero implements Comparable<Hero> {
+public class Hero extends Thread {
     /**
-     * Положение по оси X.
+     * Ссылка на экз.класса Board.
      */
-    private volatile int x;
+    private final Board mainBoard;
     /**
-     * Положение по оси Y.
+     * Ссылка на экз.класса Cell, как на то место, где расположен герой.
      */
-    private volatile int y;
+    private volatile Cell source;
     /**
-     * Имя.
+     * Ссылка на экз.класса Cell, как на то место, куда должен пойти герой.
      */
-    private final String name;
+    private Cell dest;
+    /**
+     * Шаг перемещения по оси x.
+     */
+    private int dX = 1;
+    /**
+     * Шаг перемещения по оси y.
+     */
+    private int dY = 1;
+    /**
+     * Ссылка на сканнер, для ввода пользователя.
+     */
+    private final Scanner scanner = new Scanner(System.in);
 
     /**
-     * @param pos одномерный массив со значениями положения героя по осям X и Y.
-     * @param name имя.
+     * @param name имя при создании.
+     * @param x положение по оси x.
+     * @param y положение по оси y.
+     * @param mainBoard ссылка на экз.класса Board.
      */
-    public Hero(int[] pos, String name) {
-        this.x = pos[0];
-        this.y = pos[1];
-        this.name = name;
+    public Hero(String name, int x, int y, Board mainBoard) {
+        super(name);
+        this.mainBoard = mainBoard;
+        this.source = this.mainBoard.getCells()[x][y];
     }
 
     /**
-     * @return положение по оси X.
+     * @return ссылку на ту ячейку, где находится экз.калсса Hero.
+     * Нужен для потока main, чтобы на одном поле рисовать все имеющиеся потоки.
      */
-    public int getX() {
-        return this.x;
+    public Cell getSource() {
+        return this.source;
     }
 
     /**
-     * @return положение по оси Y.
-     */
-    public int getY() {
-        return this.y;
-    }
-
-    /**
-     * @param x значение для установки нового положения по оси X.
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * @param y значение для установки нового положения по оси Y.
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    /**
-     * @param h ссылка на экземпляр объекта класса Hero для сравнения.
-     * @return 0, если экземпляры равны и 1, если - нет.
+     * Работа потока.
      */
     @Override
-    public int compareTo(Hero h) {
-        return ((this.x == h.x) && (this.y == h.y)) ? 0 : 1;
+    public void run() {
+        if (super.getName().equals("M")) { //Если имя экз.класса - "B", то поток создается с управлением для польз.
+            executeAuto(source.getX(), source.getY());
+        } else {
+            executeManual(source.getX(), source.getY());
+        }
     }
 
     /**
-     * @return строковое представление.
+     * @param x начальное положение по оси x при создании.
+     * @param y начальное положение по оси y при создании.
      */
-    public String toString() {
-        return String.format("%s [%d %d]", name, x, y);
+    private void executeAuto(int x, int y) {
+        mainBoard.getBoard()[source.getX()][source.getY()].lock(); //Сделать лок того положения, где создались.
+        while (true) {
+            if (System.currentTimeMillis() % 2 == 0) { //Если время - четное, то
+                x += dX;
+                if (mainBoard.isItSafe(x, y)) {        //Проверяется не вышел ли x за пределы диапазона.
+                    dest = mainBoard.getCells()[x][y]; //Если - нет, то присваивается.
+                } else {
+                    dX = -dX;                 //Если x вышел за пределы диапазона, то dX меняет знак на противоположный.
+                    x += 2 * dX;              //x меняет свое положение на 2 клетки назад.
+                    dest = mainBoard.getCells()[x][y];
+                }
+            } else {
+                y += dY;
+                if (mainBoard.isItSafe(x, y)) {
+                    dest = mainBoard.getCells()[x][y];
+                } else {
+                    dY = -dY;
+                    y += 2 * dY;
+                    dest = mainBoard.getCells()[x][y];
+                }
+            }
+            try {
+                if (mainBoard.move(source, dest)) {
+                    source = dest;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param x начальное положение по оси x при создании объекта.
+     * @param y начальное положение по оси y при создании объекта.
+     */
+    public void executeManual(int x, int y) {
+        mainBoard.getBoard()[source.getX()][source.getY()].lock();
+        while (true) {
+            switch (scanner.nextLine()) {
+                case "w": x--;
+                          break;
+                case "s": x++;
+                          break;
+                case "a": y--;
+                          break;
+                case "d": y++;
+                default:
+            }
+            if (mainBoard.isItSafe(x, y)) {
+                dest = mainBoard.getCells()[x][y];
+            } else {
+                continue;
+            }
+            try {
+                if (mainBoard.move(source, dest)) {
+                    source = dest;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
