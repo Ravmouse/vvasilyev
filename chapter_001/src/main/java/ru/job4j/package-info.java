93b872923b4d6524f26f497cda5262d30abd9417
Vitaly Vasilyev <rav.energ@rambler.ru>
2018-10-20 13:00:36 +0300

    upd2

--- chapter_001/src/main/java/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for Calculate.
-*
-* @author Vitaly Vasilyev (rav.energ@rambler.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
--- chapter_001/src/test/java/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for CalculateTest.
-*
-* @author Vitaly Vasilyev (rav.energ@rambler.ru)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
