4b5208cbf8d1009cb2c7ffc54e87ad143527e987
Vitaly Vasilyev <rav.energ@rambler.ru>
2018-10-29 23:05:34 +0300

    3.1. Реализовать приложения для работы с пользователем [#2513].

---
+++ chapter_008/src/main/java/ru/job4j/h3servlet/UserCreateServlet.java
@@ -0,0 +1,89 @@
+package ru.job4j.h3servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ru.job4j.h2http.UserServlet;
+
+/**
+ * Вкладка /create.
+ * @author Vitaly Vasilyev, date: 23.10.2018, e-mail: rav.energ@rambler.ru
+ * @version 1.0
+ */
+public class UserCreateServlet extends UserServlet {
+    /**
+     * При нажатии на кнопку Add остаемся на этой же странице /create, но новый user формируется.
+     * @param req запрос.
+     * @param res ответ.
+     * @throws IOException исключение.
+     */
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter pw = new PrintWriter(res.getOutputStream());
+        pw.append("<!DOCTYPE html>"
+                        + "<html lang=\"en\">"
+                            + "<head>"
+                                + "<meta charset='UTF-8'>" + "<title>Create a new user</title>"
+                            + "</head>"
+                            + "<body>"
+                                + "<h2 align='center'>Create a new user</h2>"
+                                + "<table align='center'>"
+                                    + "<form action='" + req.getContextPath() + "/create?action=add' method='POST'>"
+                                        + "<tr>"
+                                            + "<td>1. Name: </td>"
+                                            + "<td>"
+                                                + "<input type='text' name='name'>"
+                                            + "</td>"
+                                        + "</tr>"
+                                        + "<tr>"
+                                            + "<td>2. Login: </td>"
+                                            + "<td>"
+                                                + "<input type='text' name='login'>"
+                                            + "</td>"
+                                        + "</tr>"
+                                        + "<tr>"
+                                            + "<td>3. E-mail: </td>"
+                                            + "<td>"
+                                                + "<input type='text' name='email'>"
+                                            + "</td>"
+                                        + "</tr>"
+                                        + "<tr>"
+                                            + "<td>4. Date of creation: </td>"
+                                            + "<td>"
+                                                + "<input type='text' name='createDate'>"
+                                            + "</td>"
+                                        + "</tr>"
+                                        + "<tr>"
+                                            + "<td></td>"
+                                            + "<td align='center'>"
+                                                + "<input type='submit' value='Add'>" //Добавить нового пользователя.
+                                            + "</td>"
+                                        + "</tr>"
+                                    + "</form>"
+                                    + "<form action='" + req.getContextPath() + "/list'>"
+                                        + "<tr>"
+                                            + "<td></td>"
+                                            + "<td align='center'>"
+                                                + "<input type='submit' value='Show all users'>" //Показать всех пользователей.
+                                            + "</td>"
+                                        + "</tr>"
+                                    + "</form>"
+                                + "</table>"
+                            + "</body>"
+                        + "</html>");
+        pw.flush();
+    }
+
+    /**
+     * @param req запрос.
+     * @param res ответ.
+     * @throws IOException исключение.
+     */
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        super.doPost(req, res);
+        doGet(req, res);
+    }
+}
---
+++ chapter_008/src/main/java/ru/job4j/h3servlet/UserUpdateServlet.java
@@ -0,0 +1,84 @@
+package ru.job4j.h3servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ru.job4j.h2http.User;
+import ru.job4j.h2http.UserServlet;
+
+/**
+ * Вкладка /edit.
+ * @author Vitaly Vasilyev, date: 28.10.2018, e-mail: rav.energ@rambler.ru
+ * @version 1.0
+ */
+public class UserUpdateServlet extends UserServlet {
+    /**
+     * Из request получается параметр id, который при нажатии на кнопку Update, передается в doPost() страницы /list,
+     * а далее - в updateUser() класса UserServlet.
+     * @param req запрос.
+     * @param res ответ.
+     * @throws IOException исключение.
+     */
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        final PrintWriter pw = new PrintWriter(res.getOutputStream());
+        final String id = req.getParameter("id");
+        final User user = logic.findById(Integer.parseInt(id));
+        pw.append("<!DOCTYPE html>"
+                    + "<html lang=\"en\">"
+                        + "<head>"
+                            + "<meta charset='UTF-8'>" + "<title>Update the user</title>"
+                        + "</head>"
+                        + "<body>"
+                            + "<h2 align='center'>Update the user</h2>"
+                            + "<table align='center'>"
+                                + "<form action='" + req.getContextPath() + "/list?action=update&id=" + id + "' method='POST'>"
+                                    + "<tr>"
+                                        + "<td>1. Name: </td>"
+                                        + "<td>"
+                                            + "<input type='text' name='name' value='" + user.getName() + "'>"
+                                        + "</td>"
+                                    + "</tr>"
+                                    + "<tr>"
+                                        + "<td>2. Login: </td>"
+                                        + "<td>"
+                                            + "<input type='text' name='login' value='" + user.getLogin() + "'>"
+                                        + "</td>"
+                                    + "</tr>"
+                                    + "<tr>"
+                                        + "<td>3. E-mail: </td>"
+                                        + "<td>"
+                                            + "<input type='text' name='email' value='" + user.getEmail() + "'>"
+                                        + "</td>"
+                                    + "</tr>"
+                                    + "<tr>"
+                                        + "<td>4. Date of creation: </td>"
+                                        + "<td>"
+                                            + "<input type='text' name='createDate' value='" + user.getCreateDate() + "'>"
+                                        + "</td>"
+                                    + "</tr>"
+                                    + "<tr>"
+                                        + "<td></td>"
+                                        + "<td align='center'>"
+                                            + "<input type='submit' value='Update'>"
+                                        + "</td>"
+                                    + "</tr>"
+                                + "</form>"
+                            + "</table>"
+                        + "</body>"
+                    + "</html>");
+        pw.flush();
+    }
+
+    /**
+     * @param req запрос.
+     * @param res ответ.
+     * @throws IOException исключение.
+     */
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        doGet(req, res);
+    }
+}
