456cdfa1c0c07f8db3630bac7c6979a7bd089cbf
Vitaly Vasilyev <rav.energ@rambler.ru>
2019-03-31 13:19:39 +0300

    9.1.4. Архивировать проект [#861] (исправления #2).

--- chapter_009/src/main/java/ru/job4j/h1io/t4archiveproj/Args.java
+++ chapter_009/src/main/java/ru/job4j/h1io/t4archiveproj/Args.java
@@ -1,16 +1,13 @@
 package ru.job4j.h1io.t4archiveproj;
 
-import org.apache.log4j.Logger;
-import ru.job4j.utils.Utils;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.function.*;
+import java.util.function.Consumer;
 
 /**
  * @author Vitaly Vasilyev, date: 07.03.2019, e-mail: rav.energ@rambler.ru
@@ -25,10 +22,6 @@ public class Args {
      * Хэш-отображение с ключом-строкой и значением-методом.
      */
     private final Map<String, Consumer<String>> methods = new HashMap<>();
-    /**
-     * Логгер.
-     */
-    private static final Logger LOGGER = Logger.getLogger(Utils.getNameOfTheClass());
     /**
      * Директория, которую нужно заархивировать.
      */
@@ -51,7 +44,7 @@ public class Args {
      */
     public Args(String[] args) {
         if (args == null || args.length != 6) {
-            LOGGER.warn("Неверное количество аргументов!");
+            throw new RuntimeException("Неверное количество аргументов!");
         } else {
             this.args = args;
             methods.put("-d", directory());
@@ -88,10 +81,10 @@ public class Args {
                 if (path.isDirectory()) {
                     existedDir = true;
                 } else {
-                    LOGGER.warn("Это - файл, а не директория!");
+                    throw new RuntimeException("Это - файл, а не директория!");
                 }
             } else {
-                LOGGER.warn("Директория не существует!");
+                throw new RuntimeException("Директория не существует!");
             }
         };
     }
@@ -118,8 +111,11 @@ public class Args {
                         addByName(fileName, file, files);
                     }
                 } else {
-                    for (File f : file.listFiles()) {
-                        queue.offer(f);
+                    File[] dirs = file.listFiles();
+                    if (dirs != null) {
+                        for (File f : dirs) {
+                            queue.offer(f);
+                        }
                     }
                 }
             }
@@ -154,14 +150,5 @@ public class Args {
      */
     public Consumer<String> output() {
         return projectName -> this.output = projectName;
-    }
-
-    /**
-     * @param args аргументы.
-     * @throws IOException искл.
-     */
-    public static void main(String[] args) throws IOException {
-        Args a = new Args(args);
-        new Zip().output(a.output, a.path, a.files);
     }
 }
