c594554d6c7130c4450c843a5bb8799eb5894cf0
Ravmouse <rav.energ@rambler.ru>
2019-06-20 15:20:13 +0300

    Расширенное хранилище [#853] (исправления)

--- chapter_010/src/main/java/ru/job4j/h3lsp/storage/Recycle.java
+++ chapter_010/src/main/java/ru/job4j/h3lsp/storage/Recycle.java
@@ -1,49 +1,49 @@
 package ru.job4j.h3lsp.storage;
 
-import ru.job4j.h3lsp.food.Food;
 import ru.job4j.h3lsp.food.ReproduceFood;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Vitaly Vasilyev, date: 29.05.2019, e-mail: rav.energ@rambler.ru
- * @version 1.0
+ * @author Vitaly Vasilyev, date: 20.06.2019, e-mail: rav.energ@rambler.ru
+ * @version 1.1
  */
-public class Recycle extends StorageDecorator {
+public class Recycle {
     /**
-     * Список продуктов.
+     * Список перерабатываемых продуктов.
      */
-    private final List<Food> foods = new ArrayList<>();
+    private final List<ReproduceFood> foods = new ArrayList<>();
     /**
-     * Другое хранилище.
+     * Ссылка на следующее хранилище.
      */
     private Storage nextStorage;
+    /**
+     * Ссылка на хранилище-мусорку.
+     * Там реализован метод isAppropriate().
+     */
+    private final Storage trash;
 
     /**
-     * @param storage класс, реализующий интерфейс Store.
+     * @param trash мусорка.
      */
-    public Recycle(Storage storage) {
-        super(storage);
+    public Recycle(final Storage trash) {
+        this.trash = trash;
     }
 
     /**
-     * @param food продукт.
+     * @param food перерабатываемый продукт.
      */
-    @Override
-    public void add(Food food) {
+    public void add(ReproduceFood food) {
         this.foods.add(food);
     }
 
     /**
      * @param food продукт, который должен быть проверен: подходит ли он для данного хранилища или нет?
      * @return true или false.
-     * Не смотря на то, что параметр метода Food, этот класс и этот метод для продуктов типа ReproduceFood,
-     * список которых (вместо Food) будет направляться в ControlQuality.
      */
-    @Override
-    public boolean isAppropriate(Food food) {
+    public boolean isAppropriate(ReproduceFood food) {
         boolean result = false;
-        if (super.isAppropriate(food) && ((ReproduceFood) food).isCanReproduce()) {
+        if (trash.isAppropriate(food) && food.isCanReproduce()) {
             result = true;
         }
         return result;
@@ -54,31 +54,25 @@ public class Recycle extends StorageDecorator {
      * Цикл - обычный, чтобы не возникало ConcurrentModificationException, т.к. здесь осуществляется удаление из
      * коллекции, чтобы продукты для переработки не оказались в Trash в дальнейшем.
      */
-    @Override
-    public void init(List<Food> foods) {
+    public void init(List<ReproduceFood> foods) {
         for (int i = 0; i < foods.size(); i++) {
             if (isAppropriate(foods.get(i))) {
                 add(foods.get(i));
                 foods.remove(i--);
             }
         }
-        if (this.nextStorage != null) {
-            this.nextStorage.init(foods);
-        }
     }
 
     /**
      * @param nextStorage хранилище, ссылка на которое будет сохранена в объекте.
      */
-    @Override
     public void setNextStorage(Storage nextStorage) {
-        this.nextStorage = nextStorage;
+        this.nextStorage.setNextStorage(nextStorage);
     }
 
     /**
      * Показывает список продуктов.
      */
-    @Override
     public void show() {
         System.out.println("Recycle:");
         this.foods.forEach(System.out::println);
